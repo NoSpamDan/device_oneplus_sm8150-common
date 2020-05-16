1a02553a88bc83299d899f5849dbded176aa37ac
LuK1337 <priv.luk@gmail.com>
2020-05-16 12:52:40 +0200

    sm8150-common: fod: Uprev to 1.1
    Change-Id: Ia0b4c24201c4e5fc03fb5638f5281d69253f7499

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -44,7 +44,7 @@ namespace lineage {
 namespace biometrics {
 namespace fingerprint {
 namespace inscreen {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
 int dc_dimming;
@@ -202,8 +202,24 @@ Return<int32_t> FingerprintInscreen::getSize() {
     return FOD_SIZE;
 }
 
+Return<int32_t> FingerprintInscreen::getHbmOffDelay() {
+    return 0;
+}
+
+Return<int32_t> FingerprintInscreen::getHbmOnDelay() {
+    return 0;
+}
+
+Return<bool> FingerprintInscreen::supportsAlwaysOnHBM() {
+    return false;
+}
+
+Return<void> FingerprintInscreen::switchHbm(bool) {
+    return Void();
+}
+
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_1
 }  // namespace inscreen
 }  // namespace fingerprint
 }  // namespace biometrics
