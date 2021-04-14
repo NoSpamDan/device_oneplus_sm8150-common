1a7138f8f35f65c04c2312d6579e8fd76ab3b3ef
NoSpamDan <nospamdan1@gmail.com>
2021-04-13 20:05:32 -0400

    sm8150-common: Rebase on yaap
    Change-Id: I5d833692238eac35710fa5ca993803fbbd3b1031

--- common.mk
+++ common.mk
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-system \
-    $(LOCAL_PATH)/overlay-yaap
+    $(LOCAL_PATH)/overlay-candy
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# YAAP Device Settings
+# Candy Device Settings
 PRODUCT_PACKAGES += \
     DeviceSettings
 
